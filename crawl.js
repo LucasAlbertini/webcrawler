7a6e56095e1cb88a2f8e17d050a4de715f1ddc64
Lucas Albertini dos Santos <113399422+LucasAlbertini@users.noreply.github.com>
2025-11-26 23:32:14 -0300

    getUrlFromHtml, JSDOM dependency

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom');
+
 function normalizeUrl(urlString) {
     const urlObject = new URL(urlString); //URL constructor already lowercase it all
     var hostName = `${urlObject.hostname}${urlObject.pathname}`;
@@ -7,6 +9,30 @@ function normalizeUrl(urlString) {
     return hostName;
 }
 
+
+
+function getUrlsFromHtml(htmlBody, baseUrl) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a');
+    linkElements.forEach(element => {
+        let url = element.href;
+        if(element.href.startsWith('/')){
+            url = baseUrl + element.href
+        }
+        console.log(url)
+        try{
+            const normalizedUrl = normalizeUrl(url)
+            urls.push(normalizedUrl);
+        }catch{
+            console.log(`Error with url: ${url}`)
+        }
+    });
+    console.log(urls)
+    return urls
+}
+
 module.exports = { 
-    normalizeUrl 
+    normalizeUrl,
+    getUrlsFromHtml
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,22 +1,95 @@
-const {normalizeUrl} = require('./crawl');
-const {test, expect} = require('@jest/globals');
-const input = 'https://api.boot.dev/v1/courses_rest_api/learn-http/';
+const {normalizeUrl, getUrlsFromHtml} = require('./crawl');
+// const {test, expect} = require('@jest/globals');
+
+
 
 test('normalizeUrl strip protocol', () => {
+    const inputUrl = 'https://api.boot.dev/v1/courses_rest_api/learn-http';
     const expectedOutput = 'api.boot.dev/v1/courses_rest_api/learn-http';
-    const actualOutput = normalizeUrl(input);
+    const actualOutput = normalizeUrl(inputUrl);
     expect(actualOutput).toBe(expectedOutput);
 });
 
 test('normalizeUrl strip protocol and trailing slash', () => {
+    const inputUrl = 'https://api.boot.dev/v1/courses_rest_api/learn-http/';
     const expectedOutput = 'api.boot.dev/v1/courses_rest_api/learn-http';
-    const actualOutput = normalizeUrl(input);
+    const actualOutput = normalizeUrl(inputUrl);
     expect(actualOutput).toBe(expectedOutput);
 });
 
 //URL constructor lowercases the url automatically
 test('normalizeUrl capital letters', () => {
+    const inputUrl = 'https://api.BOOT.dev/v1/courses_rest_api/learn-http/';
     const expectedOutput = 'api.boot.dev/v1/courses_rest_api/learn-http';
-    const actualOutput = normalizeUrl(input);
+    const actualOutput = normalizeUrl(inputUrl);
     expect(actualOutput).toBe(expectedOutput);
+});
+
+test('getUrlsFromHtml', () => {
+    const inputHtml = `
+<html>
+    <body>
+        <a href="https://api.boot.dev/v1/courses_rest_api/learn-http/">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`;
+    const inputUrl = 'api.boot.dev/v1/courses_rest_api/learn-http';
+    const expectedOutput = ["api.boot.dev/v1/courses_rest_api/learn-http"];
+    const actualOutput = getUrlsFromHtml(inputHtml, inputUrl);
+    expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+//To be only works with primitives, since it is not checking the value, but the equality. For anything else, I should use toStrictEqual
+
+test('getUrlsFromHtml relative', () => {
+    const inputHtml = `
+<html>
+    <body>
+        <a href="/path/">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`;
+    const inputUrl = 'https://api.boot.dev/v1/courses_rest_api/learn-http';
+    const expectedOutput = ["api.boot.dev/v1/courses_rest_api/learn-http/path"];
+    const actualOutput = getUrlsFromHtml(inputHtml, inputUrl);
+    expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('getUrlsFromHtml multiple', () => {
+    const inputHtml = `
+<html>
+    <body>
+        <a href="/path/">
+            Boot.dev Blog
+        </a>
+        <a href="https://api.boot.dev/v1/courses_rest_api/learn-http/">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`;
+    const inputUrl = 'https://api.boot.dev/v1/courses_rest_api/learn-http';
+    const expectedOutput = ["api.boot.dev/v1/courses_rest_api/learn-http/path", "api.boot.dev/v1/courses_rest_api/learn-http"];
+    const actualOutput = getUrlsFromHtml(inputHtml, inputUrl);
+    expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('getUrlsFromHtml invalid', () => {
+    const inputHtml = `
+<html>
+    <body>
+        <a href="nonsense">
+            Boot.dev Blog
+        </a>
+    </body>
+</html>
+`;
+    const inputUrl = 'https://api.boot.dev/v1/courses_rest_api/learn-http';
+    const expectedOutput = [];
+    const actualOutput = getUrlsFromHtml(inputHtml, inputUrl);
+    expect(actualOutput).toStrictEqual(expectedOutput);
 });
